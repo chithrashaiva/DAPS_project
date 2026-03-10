162ce088db410ee5da35c6bf0450c5d0b26f7ec5
unknown <you@example.com>
2026-03-10 12:37:34 +0530

    Update

--- accountability_system/accountability_system/settings.py
+++ accountability_system/accountability_system/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ed)m5f^c*yh#d%fk(*_c@n6^rg3zz6_^@ii!zzdrk*1bn8qtgs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['daps-project.onrender.com']
 
